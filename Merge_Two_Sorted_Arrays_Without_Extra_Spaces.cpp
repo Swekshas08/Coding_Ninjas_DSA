91592c0d067c6f113635cb6887ea6f6aad091620
Sweksha <121374951+Swekshas08@users.noreply.github.com>
2023-08-06 23:13:53 +0530

    Create Merge_Two_Sorted_Arrays_Without_Extra_Spaces.cpp

---
+++ Merge_Two_Sorted_Arrays_Without_Extra_Spaces.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+
+void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &arr1, vector<long long> &arr2){
+	int m,n;
+    int left = n - 1;
+    int right = 0;
+
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    // Sort arr1[] and arr2[] individually:
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
+	
+}
